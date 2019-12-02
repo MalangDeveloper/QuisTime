cb23fc2413de968e3ff7d407ab4395d39f56af14
Muhammad Alfarizi <malfarizi259@gmail.com>
2019-12-02 12:51:11 +0700

    menambah Form untuk mahasiswa

---
+++ app/src/main/java/com/example/quistime/Menu/MhsLoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.quistime.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quistime.R;
+
+public class MhsLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mhs_login);
+    }
+}
